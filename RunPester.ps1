af26b16d7f966e6ac9ed19d04826cd34b2db121b
bgrauer <willig@microsoft.com>
2016-05-20 15:57:02 -0400

    revert back to original script

--- RunPester.ps1
+++ RunPester.ps1
@@ -3,9 +3,4 @@
 #
 
 Import-Module "$PSScriptRoot\Pester\Pester.psm1"
-$testResults = Invoke-Pester -CodeCoverage *.ps1 -Path "$PSScriptRoot\PSExample" -PassThru  -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResult.xml"
-
-If($testResults.FailedCount -gt 0)
-{
-    Exit 1
-}
+Invoke-Pester -CodeCoverage *.ps1 -Path "$PSScriptRoot\PSExample" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResult.xml"
